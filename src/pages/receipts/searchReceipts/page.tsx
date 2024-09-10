dc7580d7094c6641b252f619a334243a0cd0ac19
skdudu <dudusantosap@gmail.com>
2024-09-10 09:40:43 -0300

    fix: remove imports

--- src/pages/receipts/searchReceipts/page.tsx
+++ src/pages/receipts/searchReceipts/page.tsx
@@ -1,9 +1,6 @@
-import { useParams } from "react-router-dom";
-
 export default function searchReceipts(){
-    const { name } = useParams();
 
     return(
-        <h1>{name}</h1>
+        <h1>Pesquise</h1>
     )
 }
