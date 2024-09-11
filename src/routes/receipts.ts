c47f73b912ac30d187dd68ae10564dc07ec1e2d4
skdudu <dudusantosap@gmail.com>
2024-09-11 13:23:04 -0300

    update route receipt.ts

--- src/routes/receipts.ts
+++ src/routes/receipts.ts
@@ -1,21 +1,23 @@
 import axios from 'axios'
 
-export async function getReceiptsAll(){
-    try {
-        const receipts = await axios.get(`${import.meta.env.VITE_API_URL}/receipts`);
-        console.log(receipts);
-    } catch (error) {
-        console.error(error);
-    }
+export interface ReceiptProps {
+    id: string,
+    title: string,
+    description?: string,
+    restaurant_name: string,
+    tax_cover: number,
+    tax_service: number,
+    code_invitation: string
+}
+
+export async function getReceiptsAll() {
+    const response = await axios.get<ReceiptProps[]>(`${import.meta.env.VITE_API_URL}/receipts`);
+    return response.data;
 }
 
 export async function getOneReceipt(){
-    try {
-        const receipts = await axios.get(`${process.env.API_URL}/receipts/96e073f8-af07-482c-bdc8-c38a13a6d789`);
-        console.log(receipts);
-    } catch (error) {
-        console.error(error);
-    }
+    const receipts = await axios.get(`${process.env.API_URL}/receipts/96e073f8-af07-482c-bdc8-c38a13a6d789`);
+    return receipts
 }
 
 export async function createReceipt(){
