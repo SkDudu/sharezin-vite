36be843c92905976ac9a99ff1887e2c5c78b70ce
Eduardo Santos <52334473+SkDudu@users.noreply.github.com>
2024-09-21 15:38:53 -0300

    feat: add loading and empty state for receipts

--- src/pages/home/page.tsx
+++ src/pages/home/page.tsx
@@ -17,14 +17,19 @@ import imgUrl from '../../assets/nodata.svg'
 export default function Home() {
     const myUserId = "c692360d-2716-428e-99fc-12f67045736c"
     const [receipts, setReceipts] = useState<ReceiptProps[]>([])
+    const [closeReceipts, setCloseReceipts] = useState<ReceiptProps[]>([])
+    const [loading, setLoading] = useState(true)
 
     async function responseGetAllReceipts(){
         const response = await getReceiptsAll()
 
         if(response != null){
             const responsefilterReceiptsForUserOwner = response.filter(receipt => receipt.userOwner === myUserId);
-            const responsefilterReceiptsForIsClose = responsefilterReceiptsForUserOwner.filter(receipt => receipt.isClose === false)
-            setReceipts(responsefilterReceiptsForIsClose)
+            const responsefilterReceiptsForIsCloseFalse = responsefilterReceiptsForUserOwner.filter(receipt => receipt.isClose === false)
+            const responsefilterReceiptsForIsCloseTrue = responsefilterReceiptsForUserOwner.filter(receipt => receipt.isClose === true)
+            setLoading(false)
+            setReceipts(responsefilterReceiptsForIsCloseFalse)
+            setCloseReceipts(responsefilterReceiptsForIsCloseTrue)
         }else{
             return null
         }
@@ -45,7 +50,25 @@ export default function Home() {
                     <TabsTrigger value="RecibosFechados" className="w-full">Recibos fechados</TabsTrigger>
                 </TabsList>
                 <TabsContent value="meusRecibos">
-                {receipts.length > 0 ? receipts.map((receipt: ReceiptProps) => (
+                {loading ? (
+                    <Card className="mt-2">
+                        <CardHeader className="flex flex-row p-2 justify-between">
+                            <Skeleton className="w-12 h-12 rounded-md" />
+                            <Skeleton className="w-[60px] h-[20px] rounded-full" />
+                        </CardHeader>
+                        <CardContent className="flex flex-col px-2 pb-2 gap-2">
+                            <Skeleton className="w-[130px] h-[20px] rounded-full" />
+                            <Skeleton className="w-[250px] h-[10px] rounded-full" />
+                            <Skeleton className="w-[100px] h-[10px] rounded-full" />
+                            <div className="flex flex-row mt-2">
+                                <Skeleton className="w-[60px] h-[20px] rounded-full" />
+                            </div>
+                        </CardContent>
+                    </Card>
+                ) : receipts.length === 0 ? (
+                    <EmptyState path={imgUrl} title={'Sem recibos'} description={'Clique no botão de mais abaixo para adicionar um recibo.'}/>
+                ) : (
+                    receipts.map((receipt: ReceiptProps) => (
                         <Link key={receipt.id} to={`/receiptDetails/${receipt.id}`}>
                             <Card className="mb-2">
                                 <CardHeader className="flex flex-row p-2 justify-between">
@@ -72,44 +95,45 @@ export default function Home() {
                                 </CardContent>
                             </Card>
                         </Link>
-                    )) 
-                    : 
-                    <Card className="mt-2">
-                        <CardHeader className="flex flex-row p-2 justify-between">
-                            <Skeleton className="w-12 h-12 rounded-md" />
-                            <Skeleton className="w-[60px] h-[20px] rounded-full" />
-                        </CardHeader>
-                        <CardContent className="flex flex-col px-2 pb-2 gap-2">
-                            <Skeleton className="w-[130px] h-[20px] rounded-full" />
-                            <Skeleton className="w-[250px] h-[10px] rounded-full" />
-                            <Skeleton className="w-[100px] h-[10px] rounded-full" />
-                            <div className="flex flex-row mt-2">
-                                <Skeleton className="w-[60px] h-[20px] rounded-full" />
-                            </div>
-                        </CardContent>
-                    </Card>
-                    }
+                    ))
+                )}
                 </TabsContent>
                 <TabsContent value="RecibosFechados">
-                    {receipts.length < 0 ? receipts.map((receipt: ReceiptProps) => (
-                        <Link to={`/receiptDetails/${receipt.id}`}>
-                            <Card className="mt-2">
-                                <CardHeader className="flex flex-row p-2 justify-between">
-                                    <div className="flex flex-row justify-center items-center w-12 h-12 rounded-md bg-stone-100">
-                                        <Receipt color="#6b7280" weight="fill" size={32} />
-                                    </div>
-                                    <Badge variant={"default"} className="h-6 bg-gray-300">Fechado</Badge>
-                                </CardHeader>
-                                <CardContent className="flex flex-col px-2 pb-2 gap-2">
-                                    <p className="font-semibold">{receipt.title}</p>
-                                    <p>{receipt.description}</p>
-                                </CardContent>
-                            </Card>
-                        </Link>
-                    )) 
-                    : 
+                    {loading ? (
+                        <Card className="mt-2">
+                            <CardHeader className="flex flex-row p-2 justify-between">
+                                <Skeleton className="w-12 h-12 rounded-md" />
+                                <Skeleton className="w-[60px] h-[20px] rounded-full" />
+                            </CardHeader>
+                            <CardContent className="flex flex-col px-2 pb-2 gap-2">
+                                <Skeleton className="w-[130px] h-[20px] rounded-full" />
+                                <Skeleton className="w-[250px] h-[10px] rounded-full" />
+                                <Skeleton className="w-[100px] h-[10px] rounded-full" />
+                                <div className="flex flex-row mt-2">
+                                    <Skeleton className="w-[60px] h-[20px] rounded-full" />
+                                </div>
+                            </CardContent>
+                        </Card>
+                    ) : closeReceipts.length === 0 ? (
                         <EmptyState path={imgUrl} title={'Sem recibos'} description={'Clique no botão de mais abaixo para adicionar um recibo.'}/>
-                    }
+                    ) : (
+                        receipts.map((receipt: ReceiptProps) => (
+                            <Link to={`/receiptDetails/${receipt.id}`}>
+                                <Card className="mt-2">
+                                    <CardHeader className="flex flex-row p-2 justify-between">
+                                        <div className="flex flex-row justify-center items-center w-12 h-12 rounded-md bg-stone-100">
+                                            <Receipt color="#6b7280" weight="fill" size={32} />
+                                        </div>
+                                        <Badge variant={"default"} className="h-6 bg-gray-300">Fechado</Badge>
+                                    </CardHeader>
+                                    <CardContent className="flex flex-col px-2 pb-2 gap-2">
+                                        <p className="font-semibold">{receipt.title}</p>
+                                        <p>{receipt.description}</p>
+                                    </CardContent>
+                                </Card>
+                            </Link>
+                        ))
+                    )}
                 </TabsContent>
             </Tabs>
             <Link to='/createReceipt'>
