ed688c021eb6d6f0e696574f52866f0fb681a943
Eduardo Santos <52334473+SkDudu@users.noreply.github.com>
2024-09-26 22:12:22 -0300

    fix: fixing route to get historics

--- src/routes/historics.ts
+++ src/routes/historics.ts
@@ -1,24 +1,29 @@
 import axios from 'axios'
 
 export interface HistoricProps {
-    id: string,
-    nameProduct: string,
-    valueProduct: number,
-    userId: string,
+    id: string
+    nameProduct: string
+    valueProduct: string
+    userId: string
+    created_at: string
     receiptId: string
+}
+
+interface HistoricResponse {
+    historics: HistoricProps[];
 }
 
 export async function getAllHistoric(){
     const response = await axios.get<HistoricProps[]>(`${import.meta.env.VITE_API_URL}/histoticsAllReceipts`);
-    return response.data;
+    return response.data
 }
 
-export async function getHistoricByReceiptId(receiptIdParams: String) {
-    const response = await axios.get<HistoricProps[]>(`${import.meta.env.VITE_API_URL}/histoticsAllReceipts/${receiptIdParams}`)
-    return response.data;
+export async function getHistoricByReceiptId(receiptIdParams: string) {
+    const response = await axios.get<HistoricResponse>(`${import.meta.env.VITE_API_URL}/historicOfReceipt/${receiptIdParams}`);
+    return response.data.historics
 }
 
-export async function addValueInReceipt(nameProduct: string, valueProduct: number, userId: string, receiptId: string){
+export async function addValueInReceipt(nameProduct: string, valueProduct: string, userId: string, receiptId: string){
     const response = await axios.post<HistoricProps>(`${import.meta.env.VITE_API_URL}/addValueInReceipt`, {
         nameProduct, valueProduct, userId, receiptId
     })
