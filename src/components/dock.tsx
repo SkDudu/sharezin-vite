0239a62786cc2e9cbe6c4ffd1e49f41b836af605
skdudu <dudusantosap@gmail.com>
2024-11-12 12:39:08 -0300

    fix: add interface

--- src/components/dock.tsx
+++ src/components/dock.tsx
@@ -3,9 +3,17 @@ import {Button} from "@/components/ui/button.tsx";
 import {House, Plus, User} from "@phosphor-icons/react";
 import {useNavigate} from "react-router-dom";
 
-export default function dock(){
+interface DockProps{
+    screen: string,
+}
+
+export default function dock(props: DockProps){
     // eslint-disable-next-line react-hooks/rules-of-hooks
     const navigate = useNavigate()
+
+    function toHome(){
+        navigate('/home')
+    }
 
     function createReceipt(){
         navigate('/createReceipt')
@@ -17,21 +25,33 @@ export default function dock(){
 
     return (
         <div className="absolute bottom-6 right-4 left-4">
-            <Dock direction="middle" magnification={0} distance={0} className="gap-6">
+            <Dock direction="middle" magnification={0} distance={0} className="gap-6 px-4">
                 <DockIcon>
-                    <Button onClick={toProfile} variant={"default"} className="bg-white">
-                        <House size={22} color='#oboc10'/>
-                    </Button>
+                    {props.screen == "home" ?
+                        <Button onClick={toHome} variant={"default"} className="bg-blue-600 hover:bg-blue-600">
+                            <House size={22} color='#fff'/>
+                        </Button>
+                    :
+                        <Button onClick={toHome} variant={"default"} className="bg-white hover:bg-white">
+                            <House size={22} color='#oboc10'/>
+                        </Button>
+                    }
                 </DockIcon>
                 <DockIcon>
-                    <Button onClick={createReceipt} className="bg-blue-100 w-10 h-10 p-2">
+                    <Button onClick={createReceipt} className="bg-blue-100 w-10 h-10 p-2 hover:bg-blue-400">
                         <Plus size={64} color="#1e3a8a"/>
                     </Button>
                 </DockIcon>
                 <DockIcon>
-                    <Button onClick={toProfile} variant={"default"} className="bg-white">
-                        <User size={22} color='#oboc10'/>
-                    </Button>
+                    {props.screen == "profile" ?
+                        <Button onClick={toProfile} variant={"default"} className="bg-blue-600 hover:bg-blue-600">
+                            <User size={22} color='#fff'/>
+                        </Button>
+                    :
+                        <Button onClick={toProfile} variant={"default"} className="bg-white hover:bg-white">
+                            <User size={22} color='#oboc10'/>
+                        </Button>
+                    }
                 </DockIcon>
             </Dock>
         </div>
